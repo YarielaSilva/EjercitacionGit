562de810d3b35a212bba54739f32fe9b2c59b990
YarielaSilva <yarielasilva17@gmai.com>
2021-09-14 15:19:21 -0300

    Ejercicio 1 modificado, somos samuel y yariela

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,7 @@
-console.log ("haciendo cambios en la ejercitacion")
+let ejercicio1= (nombre1,nombre2)=> "Somos"+" "+nombre1+" y "+nombre2+" "+"haciendo cambios en la ejercitacion"
+
+console.log (ejercicio1("Samuel", "Yariela"))
+
+
+
+
