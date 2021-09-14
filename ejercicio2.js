8837479b456552e15523c73274747b2e64ce5919
YarielaSilva <yarielasilva17@gmai.com>
2021-09-14 15:21:07 -0300

    El ejercicio 2 es mezclar colores

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,3 +1,16 @@
-console.log ('Mi ejercicio 2')
+
+function mezclarColor(color1,color2){
+    if (color1 == "Amarillo" & color2 == "Azul"){
+        return "La mezcla es Verde"
+    }else if (color1 == "Rojo" & color2=="Amarillo"){
+        return  "la mezcla es purpura"}
+        else{
+            return "No se que color es la mezcla"}}
+
+
+
+console.log (mezclarColor("Amarillo","Azul"))
+console.log (mezclarColor("Rojo","Azul"))
+
 
 console.log ("inventando")
