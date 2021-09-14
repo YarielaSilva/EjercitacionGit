6dff97705a6f09e7065ae18dc5f8eee17437f230
YarielaSilva <yarielasilva17@gmai.com>
2021-09-14 15:22:20 -0300

    Ejer 3, El indice de la fruta

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,10 @@
+let frutas = ["Manzana","Pera","Banana"]
+
+let posicionPera =frutas.indexOf("Pera")
+
+console.log (posicionPera)
+
+
 console.log ("Mi ejercicio 3")
 
 console.log ("Nada Creativa")
